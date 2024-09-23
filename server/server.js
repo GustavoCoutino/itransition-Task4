505e2de50d209768bb7eb03dcd7c1237c7a5a795
Gustavo Coutiño <gcoutino04@gmail.com>
2024-09-22 19:54:52 -0600

    changed connection

--- server/server.js
+++ server/server.js
@@ -14,11 +14,12 @@ const PORT = process.env.PORT || 5001;
 
 app.use(bodyParser.json());
 
-const db = await mysql.createConnection({
+const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.PORT,
 });
 
 app.use((req, res, next) => {
